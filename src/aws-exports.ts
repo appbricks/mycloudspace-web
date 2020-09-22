efa526e9134a350f637666d56a174971ceedfc3f
Mevan Samaratunga <Mevan.Samaratunga2@fmr.com>
2020-09-22 17:36:11 -0400

    remove redundant file

--- src/aws-exports.ts
+++
@@ -1,8 +0,0 @@
-const awsconfig = {
-  'aws_cognito_region': 'us-east-1',
-  'aws_user_pools_id': 'us-east-1_oKgqD0W3H',
-  'aws_user_pools_web_client_id': '54b5mqn0cpdg7l4q5ilc83j2q0',
-  'oauth': {}
-};
-
-export default awsconfig;
