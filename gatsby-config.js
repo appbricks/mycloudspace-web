cfa9af95fb1890430d7b5119b3045e46f78352dd
Mevan Samaratunga <Mevan.Samaratunga2@fmr.com>
2020-09-28 15:45:58 -0400

    include content schema dir

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,6 +33,13 @@ module.exports = {
       },
     },
     {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/schema/`,
+        name: '/',
+      },
+    },
+    {
       resolve: 'gatsby-plugin-react-svg',
       options: {
         rule: {
