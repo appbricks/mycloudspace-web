10a6c4f82d80e3ad8b0f6fa24c2f479783c451f9
Mevan Samaratunga <Mevan.Samaratunga2@fmr.com>
2020-09-29 08:51:14 -0400

    updated metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,8 +50,8 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'appbricks-mycloudspace',
+        short_name: 'mycs',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
