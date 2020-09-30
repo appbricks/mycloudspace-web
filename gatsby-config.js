011fce68704715b7e5298c8190236a0d962d81a0
Mevan Samaratunga <Mevan.Samaratunga2@fmr.com>
2020-09-30 12:10:39 -0400

    StylesProvider init error

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,9 +48,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-material-ui`,
       options: {
-        stylesProvider: {
-          injectFirst: true,
-        },
+        // stylesProvider: {
+        //   injectFirst: true,
+        // },
       },
     },
     {
