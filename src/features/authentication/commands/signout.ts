8c2a0aa94b4329cac59084a21f2c0c369f96aec7
Mevan Samaratunga <msamaratunga@appbricks.io>
2020-11-11 19:43:01 -0500

    signout command

--- src/features/authentication/commands/signout.ts
+++ src/features/authentication/commands/signout.ts
@@ -1,10 +1,14 @@
 import { navigate } from '@reach/router';
 
+import { Logger } from '@appbricks/utils';
+
 import { CommandFn } from '../../../common/config';
 import * as Auth from '../../../common/state/auth';
 
 const signout: CommandFn = (dispatch, props) => {
-  dispatch(Auth.signout);
+  Logger.trace('signout', 'initiating log out action');
+
+  dispatch(Auth.signout());
   navigate(props['uri']);
 }
 
