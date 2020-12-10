0b95fe2395aa3cd183379c3a3acad12cd630b78f
Mevan Samaratunga <msamaratunga@appbricks.io>
2020-12-09 23:31:07 -0500

    added vars props to be passed to template

--- src/common/components/Content/StaticLabel.tsx
+++ src/common/components/Content/StaticLabel.tsx
@@ -2,13 +2,14 @@ import React, { FunctionComponent } from 'react'
 
 import { useLabelContent } from '../../state/content';
 
-const StaticLabel: FunctionComponent<StaticLabelProps> = ({ id }) => {
+const StaticLabel: FunctionComponent<StaticLabelProps> = ({ id, values }) => {
   const labelLookup = useLabelContent();
-  return <>{labelLookup(id).text()}</>
+  return <>{labelLookup(id).text(values)}</>
 }
 
 export default StaticLabel;
 
 type StaticLabelProps = {
-  id: string
+  id: string,
+  values?: { [name: string]: string }
 }
