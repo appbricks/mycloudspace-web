bf0919f03d87fd070b68fcf7a6cc1f5ee7f71ef4
Mevan Samaratunga <msamaratunga@appbricks.io>
2021-05-14 10:57:48 -0400

    allowed expand toggle to be just an icon or button with text

--- src/common/components/views/Tile.tsx
+++ src/common/components/views/Tile.tsx
@@ -10,6 +10,7 @@ import CardActions from '@material-ui/core/CardActions';
 import Collapse from '@material-ui/core/Collapse';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
+import Chip from '@material-ui/core/Chip';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import cx from 'clsx';
 
@@ -23,6 +24,7 @@ const Tile: FunctionComponent<TileProps> = ({
   actions,
   centerActions,
   toggleExpand,
+  toggleExpandLabel,
   expandedContent,
   children,
   ...other
@@ -32,7 +34,8 @@ const Tile: FunctionComponent<TileProps> = ({
     minWidth, 
     height, 
     minHeight,
-    centerActions
+    centerActions,
+    isExpandButton: !!toggleExpandLabel
   });
 
   const [expanded, setExpanded] = React.useState(false);
@@ -44,31 +47,50 @@ const Tile: FunctionComponent<TileProps> = ({
   return (
     <MuiCard className={styles.root}>
       <CardHeader {...header} className={cx(styles.header, insetHeader && styles.insetText)} />
-      <Divider variant="middle" />
+      <Divider variant='middle' />
       <CardContent>
         {children}
       </CardContent>
-      <Divider variant="middle" />
 
-      {actions &&
-        <CardActions disableSpacing className={styles.action}>
-          {actions}
-          {toggleExpand && 
-            <IconButton
-              size='small'
-              className={cx(styles.expand, {
-                [styles.expandOpen]: expanded,
-              })}
-              onClick={handleExpandClick}
-            >
-              <ExpandMoreIcon />
-            </IconButton>
-          }
-        </CardActions>
+      {(actions || toggleExpand) &&
+        <>
+          <Divider variant='middle' />
+          <CardActions disableSpacing className={styles.action}>
+            {actions}
+            {toggleExpand && (
+              toggleExpandLabel
+                ? <Chip
+                    label={toggleExpandLabel}
+                    clickable
+                    color='primary'
+                    variant='outlined'
+                    onClick={handleExpandClick}
+                    onDelete={handleExpandClick}
+                    deleteIcon={
+                      <ExpandMoreIcon 
+                        className={cx(
+                          styles.expand, 
+                          {[styles.expandOpen]: expanded}
+                        )}
+                      />
+                    }
+                  />
+                : <IconButton
+                    size='small'
+                    className={cx(
+                      styles.expandToggleOnly, 
+                      styles.expand, {[styles.expandOpen]: expanded})}
+                    onClick={handleExpandClick}
+                  >
+                    <ExpandMoreIcon />
+                  </IconButton>
+            )}
+          </CardActions>
+        </>
       }
 
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <Divider variant="middle" />
+      <Collapse in={expanded} timeout='auto' unmountOnExit>
+        <Divider variant='middle' />
         <CardContent>
           {expandedContent}
         </CardContent>
@@ -102,17 +124,20 @@ const useStyles = makeStyles(theme => ({
   action: (props: StyleProps) => ({
     display: 'flex',
     position: 'relative',
+    minHeight: '3.3rem',
     justifyContent: props.centerActions ? 'space-around': 'flex-end',
     marginInlineEnd: theme.spacing(0.5)
   }),
   expand: {
-    position: 'absolute',
     bottom: theme.spacing(1.4),
     right: theme.spacing(2),
     transform: 'rotate(0deg)',
     transition: theme.transitions.create('transform', {
       duration: theme.transitions.duration.shortest,
     }),
+  },
+  expandToggleOnly: {
+    position: 'absolute'
   },
   expandOpen: {
     transform: 'rotate(180deg)',
@@ -133,6 +158,7 @@ type TileProps = {
   centerActions?: boolean
 
   toggleExpand?: boolean
+  toggleExpandLabel?: string
   expandedContent?: ReactElement
 }
 
@@ -143,4 +169,6 @@ type StyleProps = {
   minHeight?: number
 
   centerActions?: boolean
+
+  isExpandButton?: boolean
 }
