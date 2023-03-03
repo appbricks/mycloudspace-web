3bc57b9449db804d33245e45a88807d1a1294c9f
Mevan Samaratunga <msamaratunga@appbricks.io>
2023-03-03 09:27:33 -0500

    fixed component name

--- src/features/devices/components/UserSettings.tsx
+++ src/features/devices/components/UserSettings.tsx
@@ -27,7 +27,7 @@ import {
 import { useStaticContent } from '../../../common/state/content';
 import { useActionStatus } from '../../../common/state/status';
 
-const MemberSettings: FunctionComponent<MemberSettingsProps> = (props) => {
+const UserSettings: FunctionComponent<UserSettingsProps> = (props) => {
   const styles = useStyles();
   const content = useStaticContent('devices', 'DeviceSettings');
 
@@ -72,7 +72,7 @@ const MemberSettings: FunctionComponent<MemberSettingsProps> = (props) => {
   );
 }
 
-export default connect(UserSpaceService.stateProps, UserSpaceService.dispatchProps)(MemberSettings);
+export default connect(UserSpaceService.stateProps, UserSpaceService.dispatchProps)(UserSettings);
 
 const useStyles = makeStyles((theme) => ({
   deleteButton: {
@@ -98,7 +98,7 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-type MemberSettingsProps =
+type UserSettingsProps =
   UserSpaceStateProps &
   UserSpaceActionProps & {
 
